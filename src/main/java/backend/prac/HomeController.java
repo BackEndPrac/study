e5f18e7aa8488c5acf962cc392077afdeebf1b0a
cousim46 <wjdghldns24@naver.com>
2022-01-08 17:37:33 +0900

    index와 포트 설정

---
+++ src/main/java/backend/prac/HomeController.java
@@ -0,0 +1,14 @@
+package backend.prac;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+}
